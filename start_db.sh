7ae72baf4382942e4f5d76e9758545978065d17f
Wim Van Leuven <wim.vanleuven@highestpoint.biz>
2015-10-16 11:51:59 +0200

    start db script

---
+++ start_db.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Start the MySQL daemon in the background.
+/usr/sbin/mysqld &
+mysql_pid=$!
+
+# Wait for MySQL deamon to start
+until mysqladmin ping &>/dev/null; do
+  echo -n "."; sleep 0.2
+done
+
+if [[ ! -z "`mysql -qfsBe "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='online_sales'" 2>&1`" ]];
+then
+  echo "DATABASE online_sales ALREADY EXISTS"
+else
+  echo "DATABASE online_sales DOES NOT EXIST"
+
+  # create the default database from the ADDed file.
+  mysql < /opt/training-src-data/scripts/vmart_schema_define.sql
+  mysql < /opt/training-src-data/scripts/vmart_data_load.sql
+fi
+
