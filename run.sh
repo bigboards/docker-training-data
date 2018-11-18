767a50e2e33de0b67fe58d13d63c510769b7d985
Wim Van Leuven <wim.vanleuven@bigboards.io>
2018-11-18 14:56:28 +0100

    use volumes for the data for improved performance

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -v ./data/docker:/var/lib/mysql training-src-data
+docker run -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -v /Users/wvl/Workspaces/BigBoards/docker-training-data/data/docker:/var/lib/mysql training-src-data
