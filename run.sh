2ee03c3afccfe0792cb9b8a305a7e39fc366207c
Wim Van Leuven <wim.vanleuven@bigboards.io>
2018-11-25 21:47:28 +0100

    fix -n flag to --name

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ THIS_DIR=`dirname "$(greadlink -f "$0")"`
 THIS_DIR=`dirname "$(readlink -f "$0")"`
 }
 
-docker run -n training-src-data -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -v $THIS_DIR/data/docker/mysql:/var/lib/mysql -v $THIS_DIR/data/docker/training-src-data:/opt/training-src-data training-src-data
+docker run -name training-src-data -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -v $THIS_DIR/data/docker/mysql:/var/lib/mysql -v $THIS_DIR/data/docker/training-src-data:/opt/training-src-data training-src-data
